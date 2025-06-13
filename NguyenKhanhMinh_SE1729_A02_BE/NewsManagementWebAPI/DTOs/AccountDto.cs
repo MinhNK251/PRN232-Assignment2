81975e222111c43660194d664a17dce000199369
Nguyễn Khánh Minh <nguyenkhanhminh251@gmail.com>
2025-06-13 17:37:02 +0700

    Change DTOs folder's location

---
+++ NguyenKhanhMinh_SE1729_A02_BE/NewsManagementWebAPI/DTOs/AccountDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsManagementWebAPI.DTOs
+{
+    public class AccountRequestDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+    public class AccountResponseDto
+    {
+        public string Token { get; set; }
+        public string UserEmail { get; set; }
+        public string UserName { get; set; }
+        public int? Role { get; set; }
+        public string AccountId { get; set; }
+    }
+}
